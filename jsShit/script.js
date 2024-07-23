e76c632f68b235489d9fffad11408411cfcd9757
hrishi-sarma <sarmahrishi05@gmail.com>
2024-07-23 19:33:09 +0530

    jfklds

---
+++ jsShit/script.js
@@ -0,0 +1,7 @@
+let query = document.querySelector('.query');
+let searchButton = document.querySelector('.go')
+
+searchButton.onclick = function(){
+    let url =  'https://www.google.com/search?q=' +query.value;
+    window.open(url, '_self');
+}
